e4beb442538693c6c55e9b8f5fdbff6f7d4fd23e
lpavezb <lukas.pavez@ug.uchile.cl>
2018-06-05 21:29:06 -0400

    :horse: add p4_b :horse:

---
+++ T1/p4/p4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+ 
+import numpy as np
+
+def p4_b(n):
+    l = 2**n
+    res = np.zeros((l, l))
+    b = True
+    for i in range(0, l):
+        for j in range(0, l):
+            if b:
+                res[i][j] = 1
+            b = not b
+        b = not b
+    return toString(res)
+
+def toString(m):
+    res = ""
+    l = np.shape(m)[0]
+    for i in range(0, l):
+        for j in range(0, l):
+            if m[i][j] == 1:
+                res += '1'
+            else:
+                res += '0'
+            res += ' '
+        res += '\n'
+    return res
+
+if __name__ == '__main__':
+    print "----------------------------------------------------"
+
+    print p4_b(2)
+    
+    print "----------------------------------------------------"
