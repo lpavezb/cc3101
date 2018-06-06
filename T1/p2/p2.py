30fbe03d19404b54c030e480011f587437c182a4
lpavezb <lukas.pavez@ug.uchile.cl>
2018-06-05 23:44:13 -0400

    :snake: add p2_c :smake:

--- T1/p2/p2.py
+++ T1/p2/p2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
- 
+
+
+###################### PARTE a ######################
+
 def p2_a(s):
     #cuasi orden: refleja y transitiva
     R = stringToMatrix(s)
@@ -7,7 +10,6 @@ def p2_a(s):
     if refleja(R) and transitiva(R):
         return 1
     return 0
-
 
 def refleja(R):
     #refleja: R(i, i) = 1
@@ -33,6 +35,43 @@ def transitiva(R):
                         break;
     return res
 
+
+
+###################### PARTE c ######################
+
+
+def p2_c(s):
+    R = stringToMatrix(s)
+    return matrixToString(RinterRinv(R))
+
+def RinterRinv(R):
+    l = len(R)
+    res = []
+    Rinv = inv(R)
+
+    for i in range(0, l):
+        res.append(['0' for i in range(0, l)])
+    for i in range(0, l):
+        for j in range(0, l):
+            if R[i][j] == Rinv[i][j] == '1':
+                res[i][j] = '1'
+    return res
+
+
+def inv(R):
+    l = len(R)
+    res = []
+    for i in range(0, l):
+        res.append(['0' for i in range(0, l)])
+    for i in range(0, l):
+        for j in range(0, l):
+            if R[i][j] == '1':
+                res[j][i] = '1'
+    return res
+
+
+###################### MISC ######################
+
 def stringToMatrix(s):
     l = s.split("\n")
     m = []
@@ -40,9 +79,25 @@ def stringToMatrix(s):
         m.append(l[i].split(" "))
     return m
 
+def matrixToString(M):
+    res = ""
+    l = len(M)
+    for i in range(0, l):
+        for j in range(0, l):
+            res += M[i][j] + " "
+        res += '\n'
+    return res
+
+
 if __name__ == '__main__':
     print "----------------------------------------------------"
+    print "-------------------- test p2_a --------------------\n"
 
     print p2_a("1 0 0\n0 1 0\n0 0 1")
-    
+
+    print "\n----------------------------------------------------"
+    print "-------------------- test p2_c --------------------\n"
+
+    print p2_c("1 1 1\n0 1 1\n0 1 1")
+
     print "----------------------------------------------------"
