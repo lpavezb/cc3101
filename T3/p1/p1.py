2aacb2f372d97cf9f9add77d2002182879ba73bf
lpavezb <lukas.pavez@ug.uchile.cl>
2018-08-23 02:19:57 -0300

    :cat:p1:cat:

--- T3/p1/p1.py
+++ T3/p1/p1.py
@@ -1,132 +1,72 @@
 #!/usr/bin/env python
+import numpy as np
 
-def p1(s):
-    letters = "qwertyuiopasdfghjklzxcvbnm"
-    const = "a"
-    var   = "b"
-    par   = "c"
-    su    = "d"
-    mult  = "e"
 
-    res = ""
+def do_tarea(m, k, par):
+    mk = pow(m, k)
+    return int(mk[par[0]][par[1]])
 
-    if not arit(s):
-        return "NO\n"
 
-    pars = countBrackets(s)
-    if pars == "NO\n":
-        return pars
+def pow(m, k):
+    if k == 1:
+        return m
+    if k % 2 == 0:
+        tmp = pow(m, k/2)
+        return tmp.dot(tmp)
+    else:
+        return pow(m, k-1).dot(m)
 
-    res += par*pars # agrega parentesis
-    res += const*countConsts(s) # agrega constantes
-    res += su*s.count("+") # agrega sumas
-    res += mult*s.count("*") # agrega multiplicaciones
 
-    for a in s: # agrega variables, suponiendo que cada variable esta compuesta por una letra
-        if a in letters:
-            res += var
-
-    res = ''.join(sorted(res)) # ordena respuesta final alfabeticamente
+def string_to_matrix(s, n):
+    # este if es por la forma en que pruebo el programa, si lo hago sin raw_input, entonces
+    # el string contiene \n, si lo hago con raw_input, entonces contiene //n
+    if '\n' in s:
+        l = s.split('\n')
+    else:
+        l = s.split('\\n')
+    m = []
+    for i in range(0, len(l)):
+        m.append(l[i].split(" "))
+    res = np.zeros((n, n))
+    for i in range(0, n):
+        for j in range(0, n):
+            if m[i][j] == '1':
+                res[i][j] = 1
     return res
 
-def arit(s):
-    letters = "qwertyuiopasdfghjklzxcvbnm"
-    num = "1234567890"
-    l = s.split("+")
-    if '' in l:
-        return False
-    b = True
-    for st in l:
-        c = False
-        if st[0] == '*' or st[len(st) - 1] == '*': return False
-        for a in st:
-            if a in letters or a in num:
-                c = True
-        b = b and c
 
-    l = s.split("*")
-    if '' in l:
-        return False
-    d = True
-    for st in l:
-        c = False
-        if st[0] == '+' or st[len(st) - 1] == '+': return False
-        for a in st:
-            if a in letters or a in num:
-                c = True
-        d = d and c
-    return b and d
+print "-----------------------------------------------------"
+print "ingrese numero de nodos en el arbol (enumerados de 1 a n): "
+print "si se presiona enter sin ingresar numero se elige n=5"
+n = raw_input("n: ")
+if n == '':
+    n = 5
+n = int(n)
+print
+print "\ningrese matriz de adyacencia"
+print "si se presiona enter sin ingresar matriz se elige 0 0 1 0 1\\n0 0 1 0 0\\n1 1 0 1 0\\n0 0 1 0 0\\n1 0 0 0 0"
+m = raw_input("matriz: ")
+if m == '':
+    m = '0 0 1 0 1\n0 0 1 0 0\n1 1 0 1 0\n0 0 1 0 0\n1 0 0 0 0'
 
-def countConsts(s):
-    num = "1234567890"
+print
+print "ingrese k: "
+print "si se presiona enter sin ingresar k se elige k=2"
+k = raw_input('k: ')
+if k == '':
+    k = 2
+k = int(k)
+print
+print "ingrese par u, v: "
+print "si se presiona enter sin ingresar par se elige (u, v) = 0 3"
+par = raw_input('u v: ')
+if par == '':
+    par = '0 3'
+p = par.split(' ')
+p[0] = int(p[0])
+p[1] = int(p[1])
 
-    n = 0
-    i = 0
-    l = len(s)
-    while i<l:
-        if s[i] in num:
-            n += 1
-            try:
-                while s[i+1] in num:
-                    i += 1
-            except Exception as e:
-                pass
-        i += 1
-    return n
-
-def countBrackets(s):
-    pars = 0
-    for a in s:
-        if a == "(":
-            pars += 1
-        if a == ")":
-            pars -= 1
-        if pars < 0:
-            return "NO\n"
-    if pars == 0:
-        return s.count("(")
-    else:
-        return "NO\n"
-
-
-def test(In, Out):
-    file_in  = open(In,"r")
-    file_out = open(Out,"r")
-
-    f = 0
-    for line in file_out:
-        line_in = file_in.readline() 
-
-        if line == "NO\n":
-            res = "NO\n"
-        else: 
-            # ordena alfabeticamente 
-            res = line.replace(",","").replace("\n","")
-            res = ''.join(sorted(res))
-
-        bol = p1(line_in) == res
-        if not bol:
-            print line_in
-            f+=1
-    print "Errores: " + str(f)
-
-
-
-
-
-if __name__ == '__main__':
-    print "-------------------------------------------------------------------------"
-    b = input("probar con un string / probar con archivos con input-outputs [0/1]: ")
-    print "-------------------------------------------------------------------------"
-    if b==0:
-        #s = "(90*r+b+(l*y)*95)*j*b*((g*51)+x)+29+g"
-        s = raw_input("ingrese string: ")
-        print "-------------------------------------------------------------------------"
-        print p1(s)
-    else:
-        In = raw_input("ingrese nombre de archivo con inputs (ej: Arit_In.txt): ")
-        Out = raw_input("ingrese nombre de archivo con outputs (ej: Arit_Out.txt): ")
-        print "-------------------------------------------------------------------------"
-        test(In, Out)
-    
-    print "-------------------------------------------------------------------------"
+matriz = string_to_matrix(m, n)
+print
+print 'respuesta: ' + str(do_tarea(matriz, k, p))
+print "-----------------------------------------------------"
